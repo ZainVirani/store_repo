92029252d54d677816f12aad72519765fdceabed
Zain Virani <drummerboyzain@rogers.com>
2014-11-25 16:15:14 -0500

    Updated login.c

--- login.c
+++ login.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
+
+int main()
+{
+	FILE* fileR = fopen("Members.csv", "r");
+	char username[1025];
+	while(!feof(fileR)){ //while end of file has not been reached
+		fgets(username, 1024, fileR); //read line
+		if(feof(fileR)) break; //if file is end of file, break while loop
+		
+	}
+	fclose(fileR); //close file
+
+}
 
 

4697af737335aea4b628b1bb4ede28e572d036eb
Alexander RUDDOCK <aruddo@teaching.CS.McGill.CA>
2014-12-02 18:27:03 -0500

    updated purchase

--- Purchase.py
+++ Purchase.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
-print ("Content-type:text/html\n")
+print "Content-Type: text/html"
+print
+
+print "<TITLE>CGI script output</TITLE>"
+print "<H1>This is my first CGI script</H1>"
+print "Hello, world!"
+
+import cgitb
+cgitb.enable()
+
 
 bill=[]
 

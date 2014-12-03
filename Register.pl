5226120ee695b5fa918c77e651946d211c8e5864
Joanna AMBROS <jambro2@teaching.CS.McGill.CA>
2014-12-02 19:32:30 -0500

    adding the confirm password button !! pull from the latest register file !!

--- Register.pl
+++ Register.pl
@@ -7,6 +7,7 @@ use warnings;
 my $username = $q->param('username');
 my $name = $q->param('name');
 my $password = $q->param('password');
+my $password1 = $q->param('password1');
 my @array = ($name, $username, $password, " ");
 my $line = join(',', @array);
 
@@ -14,6 +15,19 @@ print "Content-type: text/html\n\n";
 my $file = 'Members.csv';
 open(my $csv, '+<', $file) or die "Cannot open file";
 my $inputLine = <$csv>;
+
+	if($password ne $password1){
+	print "<HTML>\n";
+	print "<HEAD>\n";
+	print "<TITLE> Error Page </TITLE>\n";
+	print "</HAED>\n";
+	print "<BODY>\n";
+	print "<br><br><br><br>\n";
+	print "<center><b> Your passwords have to match !!! Fail. </b></center>\n";
+	print "<br><br><br><center><a href=\"register.html\"> Click here to go back to the registration page!</a></b></center> \n";
+	print "</BODY>\n";
+}
+else{
 #checking if username is already in use.
 	while($inputLine =<$csv>){
 	if(index($inputLine, $username) != -1){
@@ -31,7 +45,7 @@ my $inputLine = <$csv>;
 
 close($csv)
 }
-
+	
 else{
 #seeking to the end of the file to append if username is not in use
 seek($csv, 0, 2);
@@ -45,5 +59,6 @@ print "<b><center>Congratulations. You have made an account!</center></b>";
 print "<br><center><a href=\"login.html\">Click here to login.</center></a> \n";
 print "</BODY>\n";
 close($csv);
+}
 }
 }

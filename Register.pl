9457e01eee7b854b8f14a20f02f3ba851a0ed1e2
Joanna AMBROS <jambro2@teaching.CS.McGill.CA>
2014-12-02 19:01:08 -0500

    attempt 2091208

--- Register.pl
+++ Register.pl
@@ -1,39 +1,49 @@
-#!/usr/local/bin/perl
+#!/usr/bin/perl
 use CGI;
 my $q = CGI->new();
 use strict;
-
+use warnings;
+#getting values from the HTML form
 my $username = $q->param('username');
 my $name = $q->param('name');
 my $password = $q->param('password');
-my @array = ($name, $username, $password, "\r");
-my $line = join(' , ', @array);
+my @array = ($name, $username, $password, " ");
+my $line = join(',', @array);
 
 print "Content-type: text/html\n\n";
 my $file = 'Members.csv';
-open(FILE, "+>>$file") or die "Cannot open file $!";
-flock(FILE, 2);
-seek(FILE, 0, 0);
-my $inputLine = <FILE>;
-while(<FILE>)
-{
-	if(index($line, $username) != 4){
+open(my $csv, '+<', $file) or die "Cannot open file";
+my $inputLine = <$csv>;
+#checking if username is already in use.
+	while($inputLine =<$csv>){
+	if(index($inputLine, $username) != -1){
 	print "<HTML>\n";
 	print "<HEAD>\n";
 	print "<TITLE> Error Page </TITLE>\n";
 	print "</HEAD>\n";
 	print "<BODY>\n";
-	print "The username you have entered is already in use.";
-	print "<br><a href=\"index.html\">Home Page</a> \n";
-	print "<br><a href=\"register.html\">Registration Page</a> \n";
+	print "<br><br><br><br>\n";
+	print "<center>The username you have entered is already in use.</center>";
+	print "<br><center><a href=\"index.html\">Home Page</a></center< \n";
+	print "<br><center><a href=\"register.html\">Registration Page</a></center> \n";
 	print "</BODY>\n";
-	close(FILE);
-	}
-	else {
-#seeking to the end of the file to append
-seek(FILE, 0, 2);
-print FILE $line;
-}
-}
-close(FILE);
+	$inputLine=<$csv>;
 
+close($csv)
+}
+
+else{
+#seeking to the end of the file to append if username is not in use
+seek($csv, 0, 2);
+print $csv "$line\n";
+print "<HTML>\n";
+print "<HEAD>\n";
+print "<TITLE> Transfer Page </TITLE>\n";
+print "<BODY>\n";
+print "<br><br><br><br>\n";
+print "<b><center>Congratulations. You have made an account!</center></b>";
+print "<br><center><a href=\"login.html\">Click here to login.</center></a> \n";
+print "</BODY>\n";
+close($csv);
+}
+}

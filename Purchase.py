df5f2c3962ebd290e4d03a6fde3e02debd6e6f13
Alexander RUDDOCK <aruddo@lab1-9.CS.McGill.CA>
2014-12-04 17:37:13 -0500

    woo

--- Purchase.py
+++ Purchase.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python
-# -*- coding: UTF-8 -*-
-
-import cgi
-import cgitb
-cgitb.enable()
-
-print "Content-Type: text/plain;charset=utf-8"
-print "<h1>Well hello there</h1>"
 import cgi
 import cgitb
 cgitb.enable()

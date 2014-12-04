b0e5733c8da71ebd258251d4baddcbdf0c5ceaf7
Alexander RUDDOCK <aruddo@lab1-9.CS.McGill.CA>
2014-12-04 17:07:23 -0500

    yeeeee

--- Purchase.py
+++ Purchase.py
@@ -1,37 +1,9 @@
 #!/usr/bin/python
-print "Content-Type: text/html"
-print
+# -*- coding: UTF-8 -*-
 
-print "<TITLE>CGI script output</TITLE>"
-print "<H1>This is my first CGI script</H1>"
-print "Hello, world!"
-
+import cgi
 import cgitb
 cgitb.enable()
 
-
-bill=[]
-
-print("Please input the product numbers you wish to purchase")
-
-def add_to_list(item):
-  bill.append(item)
-  print("Added! List has {} items.".format(len(bill)))
-  
-def show_list():
-  print("Here's your list")
-  for item in bill:
-    print(item)
-    
-while True:
-  new_item = input("> ")
-  if new_item == 'DONE':
-    break
-  elif new_item == 'SHOW':
-    show_list()
-    continue
-  
-  add_to_list(new_item)
-  continue 
-  
-show_list()
+print "Content-Type: text/plain;charset=utf-8"
+print "<h1>Well hello there</h1>"

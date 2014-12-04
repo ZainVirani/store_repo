bfbfc97a2ff47c0c1edb404cec5ebd86c66adf31
Alex Ruddock <alexander.ruddock@mail.mcgill.ca>
2014-12-04 17:05:36 -0500

    Update Purchase.py

--- Purchase.py
+++ Purchase.py
@@ -10,31 +10,17 @@ print "<TITLE>CGI script output</TITLE>"
 print "<H1>This is my first CGI script</H1>"
 print "Hello, world!"
 
-
-
-
 bill=[]
+# item = a list that contains the checked items of catalogue.html 
+# with inventory.csv and if they match adds them to the bill 
 
-print("Please input the product numbers you wish to purchase")
 
 def add_to_list(item):
   bill.append(item)
-  print("Added! List has {} items.".format(len(bill)))
   
-def show_list():
-  print("Here's your list")
+def show_bill():
+  print "<h2>Here's the bill.</h2>"
   for item in bill:
     print(item)
-    
-while True:
-  new_item = input""
-  if new_item == 'DONE':
-    break
-  elif new_item == 'SHOW':
-    show_list()
-    continue
-  
-  add_to_list(new_item)
-  continue 
   
 show_list()

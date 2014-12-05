afca000bff15d9537ab71b25ab319d76a5deca5d
Alex Ruddock <alexander.ruddock@mail.mcgill.ca>
2014-12-04 20:02:58 -0500

    Update Purchase.py

--- Purchase.py
+++ Purchase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import re
 import cgi
+import csv
 
 def main():
 	print "Content-type: text/html\n"
@@ -11,28 +12,46 @@ def main():
 	#	print "<h1>Error! Please enter first name.</h1>"
 	
 	inventory = []
-	one_order = form.getvalue("one")
-	five_order = form.getvalue("five")
-	ten_order = form.getvalue("ten")
-	twenty_order = form.getvalue("twenty")
+	baby = form.getvalue("baby")
+	firm = form.getvalue("firm")
+	funny = form.getvalue("funny")
+	ganja = form.getvalue("ganja")
+	huh = form.getvalue("huh")
+	pointer = form.getvalue("pointer")
+	rasta = form.getvalue("rasta")
+	skeptic = form.getvalue("skeptic")
+	wave = form.getvalue("wave")
 	content = ""
 
 	if form.has_key("loggedin"):
 		print asdfasdf
 
 
-	with open ('../database/Inventory.scv', 'r') as f:	
+	with open ('Inventory.csv', 'r') as f:	
 		for line in f:
 			line = (line.split(","))
 			inventory.append(int(line[1]))
 
-		if ((inventory[0]-int(one_order))>=0)and((inventory[1]-int(five_order))>=0)and((inventory[2]-int(ten_order))>=0)and((inventory[3]-int(twenty_order))>=0):
-			line1 ='one,'+str(inventory[0]-int(one_order))+',1\n'
-			line2 = 'five,'+str(inventory[1]-int(five_order))+',5\n'
-			line3 = 'ten,'+str(inventory[2]-int(ten_order))+',10\n'
-			line4 = 'twenty,'+str(inventory[3]-int(ten_order))+',20\n'
+		if ((inventory[1]-int(baby))>=0)and
+		((inventory[2]-int(firm))>=0)and
+		((inventory[3]-int(funny))>=0)and
+		((inventory[4]-int(ganja))>=0)
+		((inventory[5]-int(huh))>=0)and
+		((inventory[6]-int(pointer))>=0)and
+		((inventory[7]-int(rasta))>=0)and
+		((inventory[8]-int(skeptic))>=0)and
+		((inventory[9]-int(wave))>=0):
+			line1 = 'baby,'+str(inventory[1]-int(baby))+',20\n'
+			line2 = 'firm,'+str(inventory[2]-int(firm))+',20\n'
+			line3 = 'funny,'+str(inventory[3]-int(funny))+',20\n'
+			line4 = 'ganja,'+str(inventory[4]-int(ganja))+',20\n'
+			line5 = 'huh,'+str(inventory[5]-int(huh))+',20\n'
+			line6 = 'pointer,'+str(inventory[6]-int(pointer))+',20\n'
+			line7 = 'rasta,'+str(inventory[7]-int(rasta))+',20\n'
+			line8 = 'skeptic,'+str(inventory[8]-int(skeptic))+',20\n'
+			line9 = 'wave,'+str(inventory[9]-int(wave))+',20\n'
 
-			content = line1+line2+line3+line4
+			content = line1+line2+line3+line4+line5+line6+line7+line8+line9
 			#print content
 			with open ('../database/Inventory.scv', 'w') as wf:
 				wf.write(content)

f0d2e934483f9025201796928bca74feb345a6f7
Alex Ruddock <alexander.ruddock@mail.mcgill.ca>
2014-12-07 20:27:09 -0500

    Update Purchase.py

--- Purchase.py
+++ Purchase.py
@@ -17,7 +17,7 @@ def main():
 		for line in logd:
 			line = line.strip()
 			userz.append(line)
-		if (form["username1"]!=""):
+		if (form["username1"].value!=""):
 			log = True
 	
 	if (log == True):
